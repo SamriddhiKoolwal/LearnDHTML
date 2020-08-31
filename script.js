6c73eef2c56af8115c3df3221437e7ac76e1031d
SamriddhiKoolwal <70194192+SamriddhiKoolwal@users.noreply.github.com>
2020-08-31 10:43:17 +0530

    update

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ let variable1=5;
 let variable2="sam";
 console.log(variable1);
 console.log(variable2);
+var arr=[1,2,3,4,5];
+arr[3].toString();
+document.getElementById('name').innerHTML=arr[3].toString();
