4450116f03896c548240f3c8dd42b09bada2ba06
SamriddhiKoolwal <70194192+SamriddhiKoolwal@users.noreply.github.com>
2020-08-31 10:24:39 +0530

    update script

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ myHeading.textContent = 'Hello world!';
 document.querySelector('button').onclick=function(){
   prompt("what is ur name?");
 }
+let variable1=5;
+let variable2="sam";
+console.log(variable1);
+console.log(variable2);
